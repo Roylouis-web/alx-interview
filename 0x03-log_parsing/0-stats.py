bccfb0dddc7efbd070d7371afa803bd398c38910
Roylouis-web <powellleroy488@gmail.com>
2023-07-21 05:38:58 +0000

    Algorithm found

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,16 +20,23 @@ status_code_count = [
     {'name': '405', 'count': 0},
     {'name': '500', 'count': 0}
 ]
+regex = [
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+        r'- \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]',
+        r'"GET /projects/260 HTTP/1\.1"',
+        r'(200|301|400|401|403|404|405|500) \d{1,4}$',
+]
+pattern = ' '.join(regex)
 
-regex = re.compile(r'''\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}\] "GET /projects/260 HTTP/1\.1" (200|301|400|401|403|404|405|500) \d{1,4}$
-''')
 try:
     for line in sys.stdin:
-        if re.match(regex, line) is not None:
-            t = re.findall("(?<!1\.1) \d{3} \d{1,4}", line)
+        print(re.match(pattern, line))
+        if re.match(pattern, line) is not None:
+            print(line)
+            t = re.findall(r"(?<!1\.1) \d{3} \d{1,4}", line)
             status_code, file_size = t[0].split()
             file_count += int(file_size)
-            
+
             if status_code and status_code.isdigit():
                 for status in status_code_count:
                     if status['name'] == status_code:
