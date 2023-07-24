ab7168bc381af43e2145e11043dcd8b6458fc1c8
Roylouis-web <powellleroy488@gmail.com>
2023-07-24 12:26:52 +0000

    Algorithm conplete

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,38 +20,31 @@ status_code_count = [
     {'name': '405', 'count': 0},
     {'name': '500', 'count': 0}
 ]
-status_codes = [
-        '200', '301', '400', '401',
-        '403', '404', '405', '500'
+regex = [
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+        r'- \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]',
+        r'"GET /projects/260 HTTP/1\.1"',
+        r'(200|301|400|401|403|404|405|500) \d{1,4}$',
 ]
+pattern = ' '.join(regex)
 
 try:
     for line in sys.stdin:
-        t = line.split()
-        if count == 10:
-            print(f'File size: {file_count}')
-            for status in status_code_count:
-                if status['count'] != 0:
-                    print(f'{status["name"]}: {status["count"]}')
-            count = 0
-        status_code = None
-        file_size = None
-
-        if len(t) >= 2:
-            for i in range(len(t)):
-                if t[i] in status_codes:
-                    status_code = t[i]
-                    if i + 1 < len(t):
-                        if t[i + 1].isdigit():
-                            file_size = t[i + 1]
-                elif t[i].isdigit():
-                    file_size = t[i]
-
-        file_count += int(file_size)
-
-        for status in status_code_count:
-            if status['name'] == status_code:
-                status['count'] += 1
+        if re.match(pattern, line) is not None:
+            text = line
+            if count == 10:
+                print(f'File size: {file_count}')
+                for status in status_code_count:
+                    if status['count'] != 0:
+                        print(f'{status["name"]}: {status["count"]}')
+                count = 0
+            t = re.findall(r"(?<!1\.1) \d{3} \d{1,4}", line)
+            status_code, file_size = t[0].split()
+            file_count += int(file_size)
+            if status_code and status_code.isdigit():
+                for status in status_code_count:
+                    if status['name'] == status_code:
+                        status['count'] += 1
         count += 1
 except KeyboardInterrupt:
     pass
