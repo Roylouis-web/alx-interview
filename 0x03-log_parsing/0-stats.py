2f9e58f0426668f3bfc538cc83d80bb75a88c39e
Roylouis-web <powellleroy488@gmail.com>
2023-07-24 08:39:03 +0000

    Algorithm complete

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,13 +20,10 @@ status_code_count = [
     {'name': '405', 'count': 0},
     {'name': '500', 'count': 0}
 ]
-regex = [
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
-        r'- \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]',
-        r'"GET /projects/260 HTTP/1\.1"',
-        r'(200|301|400|401|403|404|405|500) \d{1,4}$',
-]
-pattern = ' '.join(regex)
+allowed_status_codes = [
+            '200', '300', '400', '401', '403',
+            '404', '405', '500'
+        ]
 text = ''
 
 try:
@@ -38,14 +35,17 @@ try:
                 if (status['count'] != 0):
                     print(f'{status["name"]}: {status["count"]}')
             count = 0
-        if re.match(pattern, line) is not None:
-            t = re.findall(r"(?<!1\.1) \d{3} \d{1,4}", line)
-            status_code, file_size = t[0].split()
-            file_count += int(file_size)
-            if status_code and status_code.isdigit():
-                for status in status_code_count:
-                    if status['name'] == status_code:
-                        status['count'] += 1
+
+        t = line.split()
+        status_code, file_size = t[7], t[8]
+        file_count += int(file_size)
+        if (
+                status_code and status_code.isdigit()
+                and status_code in allowed_status_codes
+           ):
+            for status in status_code_count:
+                if status['name'] == status_code:
+                    status['count'] += 1
         count += 1
 except KeyboardInterrupt:
     print(f'File size: {file_count}')
