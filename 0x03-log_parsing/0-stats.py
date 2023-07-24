db4c3b7cfec5aba35b799daece9a9f0dbcf5c0df
Roylouis-web <powellleroy488@gmail.com>
2023-07-24 09:29:40 +0000

    All done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,35 +20,32 @@ status_code_count = [
     {'name': '405', 'count': 0},
     {'name': '500', 'count': 0}
 ]
-allowed_status_codes = [
-            '200', '301', '400', '401', '403',
-            '404', '405', '500'
-        ]
+regex = [
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+        r'- \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]',
+        r'"GET /projects/260 HTTP/1\.1"',
+        r'(200|301|400|401|403|404|405|500) \d{1,4}$',
+]
+pattern = ' '.join(regex)
 text = ''
 
 try:
     for line in sys.stdin:
-        text = line
-        if count == 10:
-            print(f'File size: {file_count}')
-            for status in status_code_count:
-                if (status['count'] != 0):
-                    print(f'{status["name"]}: {status["count"]}')
-            count = 0
-
-        t = line.split()
-        status_code = 0
-        file_size = 0
-        if len(t) >= 9:
-            status_code, file_size = t[7], t[8]
+        if re.match(pattern, line) is not None:
+            text = line
+            if count == 10:
+                print(f'File size: {file_count}')
+                for status in status_code_count:
+                    if status['count'] != 0:
+                        print(f'{status["name"]}: {status["count"]}')
+                count = 0
+            t = re.findall(r"(?<!1\.1) \d{3} \d{1,4}", line)
+            status_code, file_size = t[0].split()
             file_count += int(file_size)
-        if (
-                status_code and status_code.isdigit()
-                and status_code in allowed_status_codes
-           ):
-            for status in status_code_count:
-                if status['name'] == status_code:
-                    status['count'] += 1
+            if status_code and status_code.isdigit():
+                for status in status_code_count:
+                    if status['name'] == status_code:
+                        status['count'] += 1
         count += 1
 except KeyboardInterrupt:
     print(f'File size: {file_count}')
@@ -56,7 +53,10 @@ except KeyboardInterrupt:
         if status['count'] != 0:
             print(f'{status["name"]}: {status["count"]}')
 
-print(f'File size: {file_count}')
-for status in status_code_count:
-    if status['count'] != 0:
-        print(f'{status["name"]}: {status["count"]}')
+if count == 1:
+    print(f'File size: {file_count}')
+    for status in status_code_count:
+        if status['count'] != 0:
+            print(f'{status["name"]}: {status["count"]}')
+if not text:
+    print(f'File size: {file_count}')
