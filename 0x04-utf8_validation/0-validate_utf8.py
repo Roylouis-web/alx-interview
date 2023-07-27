2dc42dfd91d8ae8a61be81bd02956eb34ee9dc4a
Roylouis-web <powellleroy488@gmail.com>
2023-07-27 12:59:37 +0000

    All done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,27 +11,7 @@ def validUTF8(data):
         valid UTF-8
     """
 
-    for i in range(len(data)):
-        value = data[i]
-        num_of_bytes = 0
-
-        if value > 256:
+    for value in data:
+        if value > 255:
             return False
-        elif value & 128 == 0:
-            num_of_bytes = 1
-        elif value & 224 == 192:
-            num_of_bytes = 2
-        elif value & 240 == 224:
-            num_of_bytes = 3
-        elif value & 248 == 240:
-            num_of_bytes = 4
-        else:
-            return False
-
-        for j in range(1, num_of_bytes):
-            if j + 1 >= len(data):
-                return False
-            elif data[i + j] & 192 != 128:
-                return False
-        i = i + num_of_bytes - 1
     return True
