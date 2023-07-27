fab9d3b7f7692bbedd93af23c6885e4c3608a8f3
Roylouis-web <powellleroy488@gmail.com>
2023-07-27 11:22:21 +0000

    Algorithm complete

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
+
 """
-    Module for a function that validate for UTF-8
-    compatibility
+    Module for a function called validITF8
 """
-
-
-def convert_to_binary(num):
-    """
-        converts a number to binary
-    """
-
-    result = ""
-
-    while num != 0:
-        result += str(num % 2)
-        num = num // 2
-
-    return result
 
 
 def validUTF8(data):
     """
-        Function that validates for valid UTF-8 bytes
+        A function that checks if a series of bytes are
+        valid UTF-8
     """
 
-    result = []
+    for i in range(len(data)):
+        value = data[i]
+        num_of_bytes = 0
 
-    for i in data:
-        result.append(convert_to_binary(i))
+        if value >= 255:
+            return False
+        elif value & 128 == 0:
+            num_of_bytes = 1
+        elif value & 224 == 192:
+            num_of_bytes = 2
+        elif value & 240 == 224:
+            num_of_bytes = 3
+        elif value & 248 == 240:
+            num_of_bytes = 4
+        else:
+            return False
 
-    if all([len(string) < 9 for string in result]):
-        return True
-    return False
+        for j in range(1, num_of_bytes):
+            if j + 1 > len(data):
+                return False
+            elif data[i + j] & 192 != 1:
+                return False
+        i = i + num_of_bytes - 1
+    return True
