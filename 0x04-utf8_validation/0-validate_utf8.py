ff2dbb72ce39c3f5d500623e3e3f749d80a9db6a
Roylouis-web <powellleroy488@gmail.com>
2023-07-27 17:46:19 +0000

    Algoritm found

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -26,10 +26,11 @@ def validUTF8(data):
 
     i = 0
     while i < len(data):
+        if data[i] > 255:
+            return False
         j = check(data[i])
         k = i + j - (j != 0)
         i += 1
-
         if j == 1 or j > 4 or k > len(data):
             return False
         while i < len(data) and i <= k:
