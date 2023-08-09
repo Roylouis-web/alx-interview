ee2953f82692bc15bf569541adcd8732f29a2dff
Roylouis-web <powellleroy488@gmail.com>
2023-08-09 16:27:10 +0100

    Algorithm gotten

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+const process = require('process');
+const request = require('request');
+const arg = process.argv[2];
+
+const printCharacters = () => {
+  request(`https://swapi-api.alx-tools.com/api/films/${arg}`, async (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+
+    const characterUrls = JSON.parse(body).characters;
+    for (const characterUrl of characterUrls) {
+      const url = await printACharacter(characterUrl);
+      console.log(url);
+    }
+  });
+};
+
+const printACharacter = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+};
+
+printCharacters();
