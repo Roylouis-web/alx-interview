46005cfb82361d39e755da799c6d4a8ea827e911
Roylouis-web <powellleroy488@gmail.com>
2023-08-15 17:47:38 +0000

    Algorithm found

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(matrix):
+    """
+        a function that returns a 2d list with its values
+        shifted 90 degrees clockwise
+    """
+
+    result = []
+    le = len(matrix)
+
+    for i in range(len(matrix)):
+        temp = []
+        k = 1
+        for j in range(len(matrix)):
+            temp.append(matrix[le - k][i])
+            k += 1
+        result.append(temp)
+
+    for i in range(len(result)):
+        for j in range(len(result)):
+            matrix[i][j] = result[i][j]
