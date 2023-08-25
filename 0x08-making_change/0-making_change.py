d161a81c340ee2b0ac39aff2dff8afe4e05c1963
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 03:30:21 +0000

    Algorithm solved

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,6 +11,8 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
+    if type(total) != int:
+        return -1
     if not coins or type(coins) != list:
         return -1
     if any([coin == 0 or type(coin) != int for coin in coins]):
