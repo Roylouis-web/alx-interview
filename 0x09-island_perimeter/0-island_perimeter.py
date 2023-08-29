049af02b2b5b40da34df34376d6f6d26c16cc03a
Roylouis-web <powellleroy488@gmail.com>
2023-08-29 20:24:57 +0000

    Algorithm ound

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,7 +9,10 @@ def island_perimeter(grid):
         A function that returns the perimeter
         of the island described in grid
     """
- 
+
+    right = []
+    left = []
+
     if len(grid) > 100:
         return 0
     if any([len(g) > 100 for g in grid]):
@@ -20,6 +23,11 @@ def island_perimeter(grid):
         for j in i:
             if j == 1:
                 count += 1
+                if len(right) > 0 and len(left) > 0:
+                    if i not in right and j not in left:
+                        return 0
+                right.append(i)
+                left.append(j)
     if count:
         result = (4 * count) - ((count * 2) - 2)
     else:
