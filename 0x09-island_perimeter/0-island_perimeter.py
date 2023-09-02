7ed079d570027bc54627954bc1f1fc378c6f38a2
Roylouis-web <powellleroy488@gmail.com>
2023-09-02 14:02:17 +0100

    All done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,109 +13,68 @@ def island_perimeter(grid):
             the list of lists
     """
 
-    initial = 0
     count = 0
-
-    for i in grid:
-        for j in i:
-            if j == 1:
-                initial += 1
-
-    if initial == 1:
-        return 4
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
+                count += 4
                 if j - 1 >= 0 and j + 1 < len(grid[i]) and i + 1 < len(grid):
                     if grid[i][j - 1] == 1:
-                        count += 1
-                    elif grid[i][j + 1] == 1:
-                        count += 1
-                    elif grid[i + 1][j] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if grid[i][j + 1] == 1:
+                        count -= 1
+                    if grid[i + 1][j] == 1:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (not j - 1 >= 0 and
                       j + 1 < len(grid[i]) and i + 1 < len(grid)):
                     if grid[i][j + 1] == 1:
-                        count += 1
-                    elif grid[i + 1][j] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if grid[i + 1][j] == 1:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (j - 1 >= 0 and not
                       j + 1 < len(grid[i]) and i + 1 < len(grid)):
                     if grid[i][j - 1] == 1:
-                        count += 1
-                    elif grid[i + 1][j] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if grid[i + 1][j] == 1:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (j - 1 >= 0 and
                       j + 1 < len(grid[i]) and not i + 1 < len(grid)):
                     if grid[i][j + 1] == 1:
-                        count += 1
-                    elif grid[i][j - 1] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if grid[i][j - 1] == 1:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (j - 1 >= 0 and not
                       j + 1 < len(grid[i]) and not i + 1 < len(grid)):
                     if grid[i][j - 1] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (not j - 1 >= 0 and
                       j + 1 < len(grid[i]) and not i + 1 < len(grid)):
                     if grid[i][j + 1] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
+                            count -= 1
                 elif (not j - 1 >= 0 and not
                       j + 1 < len(grid[i]) and i + 1 < len(grid)):
                     if grid[i + 1][j] == 1:
-                        count += 1
-                    elif i - 1 >= 0:
+                        count -= 1
+                    if i - 1 >= 0:
                         if grid[i - 1][j] == 1:
-                            count += 1
-                        else:
-                            return 0
-                    else:
-                        return 0
-
-    if count:
-        result = (4 * count) - ((count * 2) - 2)
-    else:
-        result = count
-    return result
+                            count -= 1
+    return count
